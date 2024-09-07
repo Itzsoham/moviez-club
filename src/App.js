729442457a2b9ace614418bcb845259c5ca03536
kai <137367860+Itzsoham@users.noreply.github.com>
2024-09-07 20:36:19 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    semotMovies(data.Search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
